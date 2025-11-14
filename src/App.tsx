737ca620f68136bdb415dcabea897e39823c15d0
Fauzan <fauzanahsanudin@gmail.com>
2025-11-15 01:56:55 +0700

    kk

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/dashboard/Dashboard';
 import FinancialLiteracy from './pages/financialliteracy/FinancialLiteracy';
 import BehaviorWellbeing from './pages/behaviorwellbeing/BehaviorWellbeing';
@@ -8,17 +8,14 @@ import { ThemeProvider } from './contexts/ThemeContext';
 function App() {
   return (
     <ThemeProvider>
-      <Router basename="/dashboard-analisis-sigacorr">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/literacy" element={<FinancialLiteracy />} />
-          <Route path="/behavior" element={<BehaviorWellbeing />} />
-          <Route path="/regional" element={<RegionalAnalysis />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/literacy" element={<FinancialLiteracy />} />
+        <Route path="/behavior" element={<BehaviorWellbeing />} />
+        <Route path="/regional" element={<RegionalAnalysis />} />
+      </Routes>
     </ThemeProvider>
   );
 }
-
 
 export default App;
